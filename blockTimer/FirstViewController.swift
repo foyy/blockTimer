36e4ccbe17464f9ea963af38aeba0f4f1ff9461a
Hunter Leeves <hunterleeves@me.com>
2020-06-22 18:00:25 -0500

    Initial Commit

---
+++ blockTimer/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  blockTimer
+//
+//  Created by Hunter Leeves on 6/22/20.
+//  Copyright © 2020 Hunter Leeves. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
